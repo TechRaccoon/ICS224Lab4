54c92598efae7aa09c65f8bae133cc5eeac6bdcd
TechRaccoon <c0526035@online.camosun.ca>
2023-10-05 12:39:58 -0700

    first push from mac

---
+++ Lab4/Lab4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by Camilo Plaza on 2023-10-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Mom!\n";
+    return 0;
+}
